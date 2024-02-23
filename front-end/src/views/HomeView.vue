c1d48d159b68fb4d6e39a4049ab03db3710814f0
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2024-02-24 00:58:19 +0100

    switched to using vite

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,18 +1,8 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
 </script>
+
+<template>
+  <main>
+    <p class="text-center text-3xl">Hello</p>
+  </main>
+</template>
