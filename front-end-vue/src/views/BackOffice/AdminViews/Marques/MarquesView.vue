5bbeea51944e68fac1c70cbe14594efe2d5fe8f3
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2024-03-15 18:05:25 +0000

    s

--- front-end-vue/src/views/BackOffice/AdminViews/Marques/MarquesView.vue
+++ front-end-vue/src/views/BackOffice/AdminViews/Marques/MarquesView.vue
@@ -1,13 +1,17 @@
 <template>
   <button
-    class="bg-green-700 text-white rounded py-2 px-4 mx-auto my-6 hover:bg-green-800 duration-75 disabled:opacity-70 disabled:cursor-progress">
+    class="bg-green-700 text-white rounded py-2 px-4 mx-auto my-6 hover:bg-green-800 duration-75 disabled:opacity-70 disabled:cursor-progress"
+  >
     <router-link :to="{ name: 'ajouterMarque' }">
       Ajouter une Marque+
     </router-link>
   </button>
-  <Table :columns="columns" :getter="getMarquesPaginate" :deleteFrom="deletingEndpoint" :actions="actions">
-
-  </Table>
+  <Table
+    :columns="columns"
+    :getter="getMarquesPaginate"
+    :deleteFrom="deletingEndpoint"
+    :actions="actions"
+  />
 </template>
 
 <script setup>
@@ -15,13 +19,25 @@ import Table from "@/Components/Table.vue";
 import getMarquesPaginate from "@/Composables/Getters/getMarquesPaginate";
 
 // without actions
-const columns = [{ name: "ID", key: "id" }, { name: "Nom de la Marque", key: "nom" }];
+const columns = [
+  { name: "ID", key: "id" },
+  { name: "Nom de la Marque", key: "nom" },
+];
 // action
-const actions = [{ label: "Details", redirectToAction: "detailsMarque", addClass: "text-green-600" }, { label: "Modifier", redirectToAction: "modifierMarque", addClass: "text-blue-600" }, { label: "Supprimer" }];
+const actions = [
+  {
+    label: "Details",
+    redirectToAction: "detailsMarque",
+    addClass: "text-green-600",
+  },
+  {
+    label: "Modifier",
+    redirectToAction: "modifierMarque",
+    addClass: "text-blue-600",
+  },
+  { label: "Supprimer" },
+];
 
 // deleting marque
 const deletingEndpoint = "http://localhost:8000/api/marque/";
-
-
-
 </script>
