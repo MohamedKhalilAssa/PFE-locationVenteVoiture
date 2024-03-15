23308907f6ec73ec93bd891661482ad5a02dbd0f
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2024-03-15 16:05:08 +0000

    unique on migrations to avoid repetitions on admin

--- back-end/database/migrations/2024_02_24_222416_create_couleurs_voitures_table.php
+++ back-end/database/migrations/2024_02_24_222416_create_couleurs_voitures_table.php
@@ -12,8 +12,8 @@ return new class extends Migration {
     {
         Schema::create('couleurs_voitures', function (Blueprint $table) {
             $table->id();
-            $table->string('nom');
-            $table->string('Hexadecimal');
+            $table->string('nom')->unique();
+            $table->string('Hexadecimal')->nullable()->unique();
             $table->timestamps();
         });
     }
