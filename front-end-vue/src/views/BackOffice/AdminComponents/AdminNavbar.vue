2346993f9b7e5011151d6830473debc59ff809e4
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2024-04-05 02:49:09 +0000

    changing admin sidebar

--- front-end-vue/src/views/BackOffice/AdminComponents/AdminNavbar.vue
+++ front-end-vue/src/views/BackOffice/AdminComponents/AdminNavbar.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="max-h-24 flex justify-between items-center duration-100 z-50 mb-8 p-3 sticky top-0 md:p-4 md:justify-end bg-white rounded shadow-lg md:shadow-none md:bg-gray-200">
+    class="max-h-24 flex justify-between items-center duration-100 z-50 mb-8 p-3 sticky top-0 md:p-4  bg-white rounded shadow-lg ">
+    <div class="offsett"></div>
+
+    <Logo imageSrc="/assets/images/LogoPNGCroppedR.png" toName="home" class="justify-self-center" />
+
     <SideBar :addClass="show" />
-    <div id="menuToggle" class="animate__animated animate__fadeIn block pt-1 md:hidden" @click="toggleMenu">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
+
     <nav>
       <dropDown :title="$store.getters.getFullName">
         <router-link to="/profile">Profile</router-link>
@@ -21,6 +21,7 @@
 
 <script setup>
 import SideBar from "@/views/BackOffice/AdminComponents/SideBar.vue";
+import Logo from "@/Components/Logo.vue";
 import dropDown from "@/Components/DropDown.vue";
 import "animate.css";
 import { ref } from "vue";
@@ -44,5 +45,3 @@ const toggleMenu = () => {
   }
 };
 </script>
-
-<style scoped src="@/assets/css/AdminNavbar.css"></style>
--- front-end-vue/src/views/BackOffice/AdminComponents/SideBar.vue
+++ front-end-vue/src/views/BackOffice/AdminComponents/SideBar.vue
@@ -1,105 +1,101 @@
 <template>
-  <aside
-    class="-translate-x-full duration-500 ease-in-out min-h-screen min-w-52 overflow-auto w-11/12 shadow-lg sm:w-72 bg-white md:bg-gray-200 md:translate-x-0 md:shadow-none"
-    :class="props.addClass"
-  >
-    <div class="linksCont flex justify-center items-center h-min mb-12 md:mb-0">
-      <Logo imageSrc="/assets/images/LogoPNGCroppedR.png" toName="home" />
+  <aside class="fixed top-0 left-0 -translate-x-3/4 h-full bg-gray-950 w-72 shadow-sm flex flex-col"
+    :class="props.addClass" style="background-color: #333;">
+    <div class="burgerContainer flex justify-end w-full h-max p-4 pr-5 mb-12">
+      <div id="menuToggle" class="block pt-1" @click="toggleMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
-    <div
-      class="links px-6 pb-6 pl-12 flex flex-col items-center h-full overflow-auto md:pl-10"
-      style="max-height: calc(100vh - 90px)"
-    >
-      <router-link
-        :to="{ name: 'adminHome' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-square-poll-vertical h-min w-min text-2xl"></i>
-        <p>Dashboard</p>
-      </router-link>
-      <router-link
-        :to="{ name: 'usersView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-user"></i>
-        <p>Utilisateurs</p>
-      </router-link>
-      <router-link
-        :to="{ name: 'marquesView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-industry"></i>
-        <p>Marques</p>
-      </router-link>
-      <router-link
-        :to="{ name: 'modelesView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-car"></i>
-        <p>Modeles</p>
-      </router-link>
-      <router-link
-        :to="{ name: 'occasionView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-car-burst"></i>
-        <p>Occasions</p>
-      </router-link>
-      <router-link
-        to="/admin/neuf"
-        :to="{ name: 'neufView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-car-on"></i>
-        <p>Neufs</p>
-      </router-link>
-      <router-link
-        to="/admin/location"
-        :to="{ name: 'locationView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-stopwatch"></i>
-        <p>Locations</p>
-      </router-link>
-      <router-link
-        :to="{ name: 'venteView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-hand-holding-dollar"></i>
-        <p>Vente</p>
-      </router-link>
-      <router-link
-        :to="{ name: 'villesView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-city"></i>
-        <p>Villes</p>
-      </router-link>
-      <router-link
-        :to="{ name: 'couleursView' }"
-        class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100"
-      >
-        <i class="fa-solid fa-paint-roller"></i>
-        <p>Couleurs</p>
-      </router-link>
+    <div class="navIcons flex justify-end w-full overflow-auto">
+      <div class="links min-w-full pb-6 flex flex-col justify-start items-end h-full overflow-auto">
+        <router-link v-for="link in links" :to="{ name: link.to }"
+          class="adminLink text-gray-300 p-2 w-12 mx-4 my-3 flex justify-center items-center hover:font-bold duration-100">
+          <i class="fa-solid h-min w-min text-2xl" :class="'fa-' + link.icon"></i>
+        </router-link>
+      </div>
     </div>
   </aside>
+  <!-- <div class="links px-6 pb-6 pl-12 flex flex-col items-center h-full overflow-auto md:pl-10"
+    style="max-height: calc(100vh - 90px)">
+    <router-link :to="{ name: 'adminHome' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-square-poll-vertical h-min w-min text-2xl"></i>
+      <p>Dashboard</p>
+    </router-link>
+    <router-link :to="{ name: 'usersView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-user"></i>
+      <p>Utilisateurs</p>
+    </router-link>
+    <router-link :to="{ name: 'marquesView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-industry"></i>
+      <p>Marques</p>
+    </router-link>
+    <router-link :to="{ name: 'modelesView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-car"></i>
+      <p>Modeles</p>
+    </router-link>
+    <router-link :to="{ name: 'occasionView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-car-burst"></i>
+      <p>Occasions</p>
+    </router-link>
+    <router-link to="/admin/neuf" :to="{ name: 'neufView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-car-on"></i>
+      <p>Neufs</p>
+    </router-link>
+    <router-link to="/admin/location" :to="{ name: 'locationView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-stopwatch"></i>
+      <p>Locations</p>
+    </router-link>
+    <router-link :to="{ name: 'venteView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-hand-holding-dollar"></i>
+      <p>Vente</p>
+    </router-link>
+    <router-link :to="{ name: 'villesView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-city"></i>
+      <p>Villes</p>
+    </router-link>
+    <router-link :to="{ name: 'couleursView' }"
+      class="adminLink text-gray-500 p-4 w-full flex items-center space-x-3 hover:font-bold duration-100">
+      <i class="fa-solid fa-paint-roller"></i>
+      <p>Couleurs</p>
+    </router-link> -->
+  <!-- </div> -->
 </template>
 <script setup>
 import Logo from "@/Components/Logo.vue";
 
 const props = defineProps(["addClass"]);
-</script>
-<style scoped>
-aside {
-  position: fixed;
-  left: 0;
-  top: 0;
-}
 
+const links = [
+  { nom: 'Dashboard', icon: 'square-poll-vertical', to: 'adminHome' },
+  { nom: 'Utilisateurs', icon: 'user', to: 'usersView' },
+  { nom: 'Marques', icon: 'industry', to: 'marquesView' },
+  { nom: 'Modeles', icon: 'car', to: 'modelesView' },
+  { nom: 'Occasions', icon: 'car-burst', to: 'occasionView' },
+  { nom: 'Neufs', icon: 'car-on', to: 'neufView' },
+  { nom: 'Locations', icon: 'stopwatch', to: 'locationView' },
+  { nom: 'Vente', icon: 'hand-holding-dollar', to: 'venteView' },
+  { nom: 'Villes', icon: 'city', to: 'villesView' },
+  { nom: 'Couleurs', icon: 'paint-roller', to: 'couleursView' },
+]
+
+
+</script>
+<!-- <style scoped>
 .router-link-exact-active {
   background-color: rgb(254 202 202);
   border-left: solid 8px rgb(248 113 113);
   color: rgb(127 29 29);
 }
-</style>
+</style> -->
+<style scoped src="@/assets/css/AdminNavbar.css"></style>
