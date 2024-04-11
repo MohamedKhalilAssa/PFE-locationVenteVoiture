d8b0d414a1c05fd646da5c0ce67463838b4a0dc9
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2024-04-11 15:19:59 +0000

    add image preparing

--- front-end-vite-vue/src/Components/addImageField.vue
+++ front-end-vite-vue/src/Components/addImageField.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="image-field">
-    <label for="img" id="image-upload"
-      ><i class="fa-solid fa-plus plus-icon"></i
-    ></label>
-    <p id="img-para">Add image(png,jpg,jpeg...)</p>
-    <input id="img" class="input" type="file" name="image" />
-  </div>
+  
 </template>
 
 <script setup>
@@ -23,44 +17,5 @@ let theImage = document.querySelector("#image-upload img");
 // });
 </script>
 <style scoped>
-/* Image Styling */
-#image-upload {
-  display: inline-block;
-  width: 90px;
-  height: 90px;
-  border: 5px solid var(--input-color-gray);
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  margin: auto;
-  overflow: hidden;
-}
-.plus-icon {
-  width: 50px;
-  height: 50px;
-  opacity: 0.7;
-  object-fit: cover;
-}
-#img {
-  display: none;
-}
-.image-field {
-  display: none;
-  margin-bottom: 0rem;
-  transition: all 0.3s ease-in;
-  overflow: hidden;
-}
-#img-para {
-  width: 100%;
-  display: inline-block;
-  margin-top: 0.5rem;
-  text-align: center;
-}
-.text-center {
-  text-align: center;
-  width: 100%;
-}
-/* ends */
+
 </style>
