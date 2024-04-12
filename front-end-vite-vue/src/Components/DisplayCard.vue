61b229c9b615fefb2f394a652333408dfaed2d8d
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2024-04-12 23:26:02 +0000

    start card design

---
+++ front-end-vite-vue/src/Components/DisplayCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="card-container bg-white shadow border overflow-hidden w-3/4 h-44 mx-auto p-2"
+  >
+    <div class="img-container border-2 border-black w-1/6 h-full"></div>
+  </div>
+</template>
--- front-end-vite-vue/src/views/FrontOffice/Occasion/OccasionListingsView.vue
+++ front-end-vite-vue/src/views/FrontOffice/Occasion/OccasionListingsView.vue
@@ -1,11 +1,9 @@
 <template>
-    <main>
-        
-    </main>
+  <main class="py-10">
+    <DisplayCard></DisplayCard>
+  </main>
 </template>
 <script setup>
-
+import DisplayCard from "@/Components/DisplayCard.vue";
 </script>
-<style >
-    
-</style>
+<style></style>
