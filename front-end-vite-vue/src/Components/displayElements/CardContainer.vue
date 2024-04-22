55d54cddfd8df126aa4861c3f540ac6ce1be3f3e
MohamedKhalilAssa <assaddiki.mohamed1@gmail.com>
2024-04-22 18:01:42 +0100

    sorting for display

--- front-end-vite-vue/src/Components/displayElements/CardContainer.vue
+++ front-end-vite-vue/src/Components/displayElements/CardContainer.vue
@@ -10,6 +10,28 @@
     class="py-10 pt-28 px-12 flex flex-col justify-between items-center lg:pt-0"
   >
     <div class="content flex flex-wrap justify-center gap-7">
+      <div
+        class="relative sm:pr-24 m-2 w-full rounded-lg flex flex-col-reverse justify-end gap-4 sm:flex-row"
+      >
+        <select
+          class="h-10 rounded-sm p-2 z-0 border border-black w-full sm:max-w-max"
+          v-model="requestParams.sort"
+          @change="sort"
+        >
+          <option selected value="none">Sort By</option>
+          <option
+            scope="row"
+            v-for="state in [
+              { key: 'asc', name: 'Par prix croissant' },
+              { key: 'desc', name: 'Par prix décroissant' },
+            ]"
+            :key="state.key"
+            :value="state.key"
+          >
+            {{ state.name }}
+          </option>
+        </select>
+      </div>
       <DisplayCard
         v-for="card in result.data"
         :key="card.id"
@@ -41,9 +63,9 @@ const result = ref([]);
 
 const requestParams = ref({
   sort: "none",
-  sortColumn: "id",
+  sortColumn: props.type == "location" ? "prix_location" : "prix_vente",
   search: "",
-  searchColumn: "nom",
+  searchColumn: "",
 });
 const currentFilters = ref({});
 
@@ -52,6 +74,14 @@ getPaginate(1, props.getter, requestParams.value, "").then((data) => {
     result.value = data.PaginateQuery;
   }
 });
+
+const sort = () => {
+  getPaginate(1, props.getter, requestParams.value).then((data) => {
+    if (data) {
+      result.value = data.PaginateQuery;
+    }
+  });
+};
 
 // for pagination
 const updateResult = (data) => {
